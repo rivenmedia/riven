040db6e02ffb71c671eb9e52d34a963faec0c2d9
Spoked <Spoked@localhost>
2023-12-10 14:49:41 -0500

    remove old controllers. add lifespan to main.

--- backend/main.py
+++ backend/main.py
@@ -5,14 +5,20 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from program.program import Program
 from utils.thread import ThreadRunner
-from controllers.controller import PlexController, ContentController
 from controllers.settings import settings_router
 from controllers.items import items_router
 
 
 sys.path.append(os.getcwd())
+program = Program()
+runner = ThreadRunner(program.run, 5)
 
-app = FastAPI()
+def lifespan(app: FastAPI):
+    runner.start()
+    yield
+    runner.stop()
+
+app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -21,21 +27,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-program = Program()
 app.program = program
 app.include_router(settings_router)
 app.include_router(items_router)
-app.include_router(PlexController(app).router)
-app.include_router(ContentController(app).router)
-
-runner = ThreadRunner(program.run, 5)
 
 if __name__ == "__main__":
-
-    try:
-        runner.start()
-        uvicorn.run(app, host="localhost", port=8080)
-    except KeyboardInterrupt:
-        print("Shutting down...")
-        runner.stop()
-        sys.exit(0)
+    uvicorn.run(app, host="localhost", port=8080)
