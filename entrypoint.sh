bfc1834b394160a95858f1461896ec44443cece7
Spoked <Spoked@localhost>
2023-12-17 01:14:41 -0500

    Use existing user/group if they already exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,34 +1,27 @@
 #!/bin/sh
 
-# Check and set default values for PUID and PGID if not provided
-PUID=${PUID:-1000}
-PGID=${PGID:-1000}
+echo "Starting Container with ${PUID}:${PGID} permissions..."
 
-echo "Starting Container with $PUID:$PGID permissions..."
-
-# Check if the iceberg user or group exists, and delete if they do
-if getent passwd iceberg > /dev/null 2>&1; then
-    deluser iceberg
-fi
-if getent group iceberg > /dev/null 2>&1; then
-    delgroup iceberg
-fi
-
-# Create the iceberg group if it doesn't exist
-if ! getent group $PGID > /dev/null 2>&1; then
+# Create group if it doesn't exist or reuse the existing group
+if ! getent group $PGID > /dev/null; then
     addgroup -g $PGID iceberg
 else
-    iceberg_group=$(getent group $PGID | cut -d: -f1)
-    echo "Group with GID $PGID already exists as $iceberg_group"
+    existing_group=$(getent group $PGID | cut -d: -f1)
+    echo "Group with GID $PGID already exists as $existing_group"
+    iceberg_group=$existing_group
 fi
 
-# Create the iceberg user
-if ! getent passwd $PUID > /dev/null 2>&1; then
-    adduser -D -u $PUID -G iceberg iceberg
+# Create user if it doesn't exist or reuse the existing user
+if ! getent passwd $PUID > /dev/null; then
+    adduser -D -u $PUID -G ${iceberg_group:-iceberg} iceberg
 else
-    iceberg_user=$(getent passwd $PUID | cut -d: -f1)
-    echo "User with UID $PUID already exists as $iceberg_user"
+    existing_user=$(getent passwd $PUID | cut -d: -f1)
+    echo "User with UID $PUID already exists as $existing_user"
+    iceberg_user=$existing_user
 fi
 
-chown -R iceberg:iceberg /iceberg
-exec su iceberg -c "$@"
+# Change ownership of relevant directories
+chown -R ${PUID}:${PGID} /iceberg
+
+echo "Initialization complete. Executing main process..."
+exec su -s /bin/sh -c "$@" ${iceberg_user:-iceberg}
