f4b35d6892cdbdec21507f25a3bf493679430a50
Spoked <5782630+dreulavelle@users.noreply.github.com>
2023-12-19 04:28:37 -0500

    Docker (#70)
    * add makefile. fix entrypoint. dockerfile needs entrypoint execution tweak though.
    
    * Fixed entrypoint. Fixed user being set to root
    
    * Fix readme
    
    * Done. Can now set PUID/PGID correctly. Updated Dev Env makefile.
    
    * Sneak in update lol
    
    * Update help
    
    * Correct syntax for update.
    
    ---------
    
    Co-authored-by: Spoked <Spoked@localhost>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Starting Container with ${PUID}:${PGID} permissions..."
+echo "Starting Container with ${PUID:-1000}:${PGID:-1000} permissions..."
 
-# Create group if it doesn't exist or reuse the existing group
-if ! getent group $PGID > /dev/null; then
-    addgroup -g $PGID iceberg
-else
-    existing_group=$(getent group $PGID | cut -d: -f1)
-    echo "Group with GID $PGID already exists as $existing_group"
-    iceberg_group=$existing_group
+if ! [ "$PUID" -eq "$PUID" ] 2> /dev/null; then
+    echo "PUID is not a valid integer. Exiting..."
+    exit 1
 fi
 
-# Create user if it doesn't exist or reuse the existing user
-if ! getent passwd $PUID > /dev/null; then
-    adduser -D -u $PUID -G ${iceberg_group:-iceberg} iceberg
-else
-    existing_user=$(getent passwd $PUID | cut -d: -f1)
-    echo "User with UID $PUID already exists as $existing_user"
-    iceberg_user=$existing_user
+if ! [ "$PGID" -eq "$PGID" ] 2> /dev/null; then
+    echo "PGID is not a valid integer. Exiting..."
+    exit 1
 fi
 
-# Change ownership of relevant directories
-chown -R ${PUID}:${PGID} /iceberg
+: ${USERNAME:=iceberg}
+: ${GROUPNAME:=iceberg}
 
-echo "Initialization complete. Executing main process..."
-exec su -s /bin/sh -c "$@" ${iceberg_user:-iceberg}
+
+if ! getent group ${PGID} >/dev/null; then
+    addgroup -g $PGID $GROUPNAME > /dev/null
+else
+    GROUPNAME=$(getent group ${PGID} | cut -d: -f1)
+fi
+
+if ! getent passwd ${PUID} >/dev/null; then
+    adduser -D -u $PUID -G $GROUPNAME $USERNAME > /dev/null
+else
+    USERNAME=$(getent passwd ${PUID} | cut -d: -f1)
+fi
+
+chown -R ${USERNAME}:${GROUPNAME} /iceberg
+
+echo "Container Initialization complete."
+exec su -m $USERNAME -c 'cd backend && source /venv/bin/activate && exec python /iceberg/backend/main.py & node /iceberg/frontend/build'
