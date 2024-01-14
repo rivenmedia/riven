6127d3dbb62688f6b3c57495912c76c7365486d6
Spoked <Spoked@localhost>
2024-01-14 01:05:15 -0500

    Update sort logic

--- backend/utils/parser.py
+++ backend/utils/parser.py
@@ -20,26 +20,28 @@ class Parser:
         self.settings = ParserConfig(**settings_manager.get("parser"))
         self.language = self.settings.language or ["English"]
         self.resolution = ["1080p", "720p"]
-        self.unwanted_codec = ["H.265 Main 10", "H.265", "H.263", "Xvid"]  # Bad for transcoding
+        self.unwanted_codec = ["H.263", "Xvid"]  # Bad for transcoding
         self.unwanted_quality = ["Cam", "Telesync", "Telecine", "Screener", 
                                  "DVDSCR", "Workprint", "DVD-Rip", "TVRip", 
                                  "VODRip", "DVD-R", "DSRip", "BRRip"]
         self.quality = [None, "Blu-ray", "WEB-DL", "WEBRip", "HDRip", 
                         "HDTVRip", "BDRip", "Pay-Per-View Rip"]
-        self.audio = [None, "AAC", "AAC 2.0", "FLAC", "AVC", "Custom"]
+        self.audio = [None, "AAC", "AAC 2.0", "AAC 5.1", "FLAC", "AVC", "Custom"]
         self.network = ["Apple TV+", "Amazon Studios", "Netflix", 
                         "Nickelodeon", "YouTube Premium", "Disney Plus", 
                         "DisneyNOW", "HBO Max", "HBO", "Hulu Networks", 
                         "DC Universe", "Adult Swim", "Comedy Central", 
-                        "Peacock", "AMC", "PBS", "Crunchyroll", "Syndication"]  # Will probably be used later in `Versions`
+                        "Peacock", "AMC", "PBS", "Crunchyroll", 
+                        "Syndication", "Hallmark", "BBC", "VICE",
+                        "MSNBC", "Crave"]  # Will probably be used later in `Versions`
         self.validate_settings()
 
     def validate_settings(self):
         if self.settings.highest_quality:
             self.resolution = ["UHD", "2160p", "4K", "1080p", "720p"]
-            self.unwanted_codec -= ["H.265 Main 10", "H.265"]
             self.audio += ["Dolby TrueHD", "Dolby Atmos",
                           "Dolby Digital EX", "Dolby Digital Plus",
+                          "Dolby Digital 5.1", "Dolby Digital 7.1",
                           "Dolby Digital Plus 5.1", "Dolby Digital Plus 7.1"
                           "DTS-HD MA", "DTS-HD MA", "DTS-HD", "DTS-HD MA 5.1"
                           "DTS-EX", "DTS:X", "DTS", "5.1", "7.1"]
@@ -114,7 +116,7 @@ class Parser:
         """Check if content is `4k`."""
         if self.settings.include_4k:
             parsed = self._parse(string)
-            return parsed.get("resolution", False) in ["4K"]
+            return parsed.get("resolution", False) in ["2160p", "4K"]
 
     def _is_highest_quality(self, string) -> bool:
         """Check if content is `highest quality`."""
@@ -124,7 +126,8 @@ class Parser:
                 parsed.get("hdr", False),
                 parsed.get("remux", False),
                 parsed.get("audio", False) in self.audio,
-                parsed.get("resolution", False) in ["4K", "UHD"]
+                parsed.get("resolution", False) in ["UHD", "2160p", "4K"],
+                parsed.get("upscaled", False)
             ])
 
     def _is_repack_or_proper(self, string) -> bool:
@@ -142,8 +145,11 @@ class Parser:
             parsed = self._parse(string)
             return parsed.get("audio") == "Dual" or \
                    re.search(r"((dual.audio)|(english|eng)\W+(dub|audio))", string, flags=re.IGNORECASE) is not None
-        else:
-            return False
+
+    def _is_network(self, string) -> bool:
+        """Check if content is from a `network`."""
+        parsed = self._parse(string)
+        return parsed.get("network", False) in self.network
 
     def sort_streams(self, streams: dict) -> dict:
         """Sorts streams based on user preferences."""
@@ -154,16 +160,19 @@ class Parser:
                 self._is_dual_audio(title),
                 self._is_repack_or_proper(title),
                 self._is_highest_quality(title),
-                self._is_4k(title)
+                self._is_4k(title),
+                self._is_network(title)
             )
         sorted_streams = sorted(streams.items(), key=sorting_key, reverse=True)
         return dict(sorted_streams)
 
     def parse(self, string) -> bool:
+        """Parse the given string and return True if it matches the user settings."""
         parse = self._parse(string)
         return (
             parse["resolution"] in self.resolution
             and parse["language"] in self.language
+            and parse["audio"] in self.audio
             and not parse["quality"] in self.unwanted_quality
             and not parse["codec"] in self.unwanted_codec
         )
