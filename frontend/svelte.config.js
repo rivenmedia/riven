18546ea65e924944eaf700177bb396100b9cd2f9
Ayush Sehrawat <itsayushsehrawat@gmail.com>
2024-01-23 15:01:02 +0530

    feat: deps change

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,11 +1,8 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { preprocessMeltUI, sequence } from '@melt-ui/pp';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: sequence([vitePreprocess({}), preprocessMeltUI()]),
-
+	preprocess: vitePreprocess({}),
 	kit: {
 		adapter: adapter(),
 		alias: {
