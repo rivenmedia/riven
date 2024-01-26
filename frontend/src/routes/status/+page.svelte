948735dc38f8592a9094d8f0712b99d775c793de
Ayush Sehrawat <itsayushsehrawat@gmail.com>
2024-01-26 23:27:16 +0530

    feat: status page improvements

--- frontend/src/routes/status/+page.svelte
+++ frontend/src/routes/status/+page.svelte
@@ -3,8 +3,9 @@
 	import { convertIcebergItemsToObject, formatDate, formatWords } from '$lib/helpers';
 	import { goto, invalidate } from '$app/navigation';
 	import { Button } from '$lib/components/ui/button';
+	import { Badge } from '$lib/components/ui/badge';
 	import * as Tooltip from '$lib/components/ui/tooltip';
-	import { Loader2, ArrowUpRight, RotateCw, Info } from 'lucide-svelte';
+	import { Loader2, ArrowUpRight, RotateCw, MoveUpRight } from 'lucide-svelte';
 	import { toast } from 'svelte-sonner';
 	import type { StatusInfo } from '$lib/types';
 	import * as Carousel from '$lib/components/ui/carousel/index.js';
@@ -113,9 +114,12 @@
 				<Carousel.Root opts={{ dragFree: true }} class="w-full max-w-full flex flex-col gap-4">
 					<div class="flex items-center justify-between">
 						<div class="flex flex-col">
-							<h3 class="text-xl md:text-2xl font-semibold">
-								{statusInfo[key].text ?? formatWords(key)}
-							</h3>
+							<a href="/status/type/{key}" class="flex gap-1 items-center hover:underline">
+								<h3 class="text-xl md:text-2xl font-semibold">
+									{statusInfo[key].text ?? formatWords(key)}
+								</h3>
+								<MoveUpRight class="size-4 md:size-6" />
+							</a>
 							<p class="text-muted-foreground text-sm">{statusInfo[key].description}</p>
 						</div>
 						<div class="flex items-center justify-center gap-2 mt-6">
@@ -127,18 +131,28 @@
 						{#each icebergItems[key] as item}
 							<Carousel.Item class="flex-none mr-2 min-w-0 max-w-max w-full h-full group/item">
 								<div class="flex flex-col w-full h-full max-w-[144px] md:max-w-[176px]">
-									<img
-										alt={item.imdb_id}
-										class="bg-cover bg-center h-[216px] w-[144px] md:w-[176px] md:h-[264px] rounded-md border-muted group-hover/item:scale-105 duration-300 transition-all ease-in-out"
-										src={`https://images.metahub.space/poster/small/${item.imdb_id}/img`}
-									/>
-									<a
-										href="/status/item/{item.item_id}"
+									<div class="relative h-full w-full">
+										<img
+											alt={item.imdb_id}
+											class="bg-cover bg-center h-[216px] w-[144px] md:w-[176px] md:h-[264px] rounded-md border-muted group-hover/item:scale-105 duration-300 transition-all ease-in-out"
+											src={`https://images.metahub.space/poster/small/${item.imdb_id}/img`}
+										/>
+										<div class="absolute top-2 left-2">
+											<Badge class="rounded-md bg-opacity-40 backdrop-blur-lg drop-shadow-lg">
+												{item.type === 'movie' ? 'Movie' : 'TV Show'}
+											</Badge>
+										</div>
+									</div>
+									<p
 										class="text-start text-sm mt-2 text-ellipsis line-clamp-1 group-hover/item:underline focus:underline"
-										>{item.title}</a
 									>
+										{item.title}
+									</p>
 									<p class="text-muted-foreground text-xs mt-1">
 										{formatDate(item.aired_at, 'year')}
+									</p>
+									<p class="text-muted-foreground text-xs mt-1">
+										Requested {formatDate(item.requested_at, 'long', true)}
 									</p>
 								</div>
 							</Carousel.Item>
