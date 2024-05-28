9f42504ad844f4c6f9999c9b37f10e892ff0a7c8
David <davidisadev@gmail.com>
2024-05-28 17:59:30 -0500

    fix (frontend): allows you to pass through onboarding steps

--- frontend/src/routes/settings/content/+page.server.ts
+++ frontend/src/routes/settings/content/+page.server.ts
@@ -45,7 +45,7 @@ export const actions: Actions = {
 
 		try {
 			const data = await setSettings(event.fetch, toSet, contentSettingsServices);
-			if (!data.allServicesTrue) {
+			if (!data.data.success) {
 				return message(
 					form,
 					`${contentSettingsServices.map(formatWords).join(', ')} service(s) failed to initialize. Please check your settings.`,
