666a1ee4c1da6b3785b8aab2c346e3155371ce05
Davide Marcoli <69892203+davidemarcoli@users.noreply.github.com>
2024-06-06 21:19:54 +0200

    Jackett improvements (#349)
    * Start with jackett improvements (WIP)
    
    * First working improved jackett scraper
    
    * Remove logs
    
    * Validate if there are indexers configured
    
    * Change log level

--- backend/program/scrapers/jackett.py
+++ backend/program/scrapers/jackett.py
@@ -1,5 +1,11 @@
 """ Jackett scraper module """
 
+import requests
+import threading
+import time
+import xml.etree.ElementTree as ET
+import queue
+
 from typing import Dict, Generator
 
 from program.media.item import MediaItem, Show
@@ -11,6 +17,15 @@ from RTN.exceptions import GarbageTorrent
 from utils.logger import logger
 from utils.request import RateLimiter, RateLimitExceeded, get, ping
 
+class JackettIndexer:
+    def __init__(self):
+        self.title = None
+        self.id = None
+        self.link = None
+        self.type = None
+        self.language = None
+        self.tv_search_capatabilities = None
+        self.movie_search_capatabilities = None
 
 class Jackett:
     """Scraper for `Jackett`"""
@@ -41,10 +56,11 @@ class Jackett:
         if self.settings.url and self.settings.api_key:
             self.api_key = self.settings.api_key
             try:
-                url = f"{self.settings.url}/api/v2.0/indexers/!status:failing,test:passed/results/torznab?apikey={self.api_key}&cat=2000&t=movie&q=test"
-                response = ping(url=url, timeout=60)
-                if response.ok:
-                    return True
+                if len(self._get_indexers()) == 0:
+                    logger.error("No Jackett indexers configured.")
+                    return False
+
+                return True
             except ReadTimeout:
                 logger.exception("Jackett request timed out. Check your indexers, they may be too slow to respond.")
                 return False
@@ -86,59 +102,198 @@ class Jackett:
 
     def api_scrape(self, item: MediaItem) -> tuple[Dict[str, Torrent], int]:
         """Wrapper for `Jackett` scrape method"""
-        with self.minute_limiter:
-            query = ""
+
+        indexers = self._get_indexers()
+
+        threads = []
+        results_queue = queue.Queue()  # Create a Queue instance to hold the results
+
+        # Define a wrapper function that calls the actual target function and stores its return value in the queue
+        def thread_target(item: MediaItem, indexer):
+            logger.debug(f"Searching on {indexer.title}")
+            start_time = time.time()
+
+            # Call the actual function
             if item.type == "movie":
-                if not hasattr(item.aired_at, "year") or not item.aired_at.year:
-                    query = f"cat=2000&t=movie&q={item.title}"
-                else:
-                    query = f"cat=2000&t=movie&q={item.title}&year={item.aired_at.year}"
-            elif item.type == "season":
-                query = f"cat=5000&t=tvsearch&q={item.parent.title}&season={item.number}"
-            elif item.type == "episode":
-                query = f"cat=5000&t=tvsearch&q={item.parent.parent.title}&season={item.parent.number}&ep={item.number}"
-            
-            url = f"{self.settings.url}/api/v2.0/indexers/all/results/torznab?apikey={self.api_key}&{query}"
-            
-            with self.second_limiter:
-                response = get(url=url, retry_if_failed=False, timeout=60)
-            
-            if not response.is_ok or len(response.data.get("rss", {}).get("channel", {}).get("item", [])) <= 0:
-                return {}, 0
-            
-            streams = response.data["rss"]["channel"].get("item", [])
-            if not streams:
-                return {}, 0
-            
-            torrents = set()
-            correct_title = item.get_top_title()
-            if not correct_title:
-                logger.debug(f"Correct title not found for {item.log_string}")
-                return {}, 0
-            
-            for stream in streams:
-                try:
-                    attr = stream.get("torznab:attr", [])
-                    infohash_attr = next((a for a in attr if a.get("@name") == "infohash"), None)
-                    if not infohash_attr:
-                        continue
-                    infohash = infohash_attr.get("@value")
-                except (TypeError, ValueError, AttributeError):
-                    continue
+                result = self._search_movie_indexer(item, indexer)
+            elif item.type == "season" or item.type == "episode":
+                result = self._search_series_indexer(item, indexer)
+            else:
+                raise TypeError("Only Movie and Series is allowed!")
 
-                if self.hash_cache.is_blacklisted(infohash):
-                    continue
+            logger.debug(
+                f"Search on {indexer.title} took {time.time() - start_time} seconds and found {len(result)} results")
 
-                try:
-                    torrent: Torrent = self.rtn.rank(
-                        raw_title=stream.get("title"), infohash=infohash, correct_title=correct_title, remove_trash=True
-                    )
-                except GarbageTorrent:
-                    continue
-                
-                if torrent and torrent.fetch:
-                    torrents.add(torrent)
+            results_queue.put(result)  # Put the result in the queue
+
+        for indexer in indexers:
+            # Pass the wrapper function as the target to Thread, with necessary arguments
+            threads.append(threading.Thread(target=thread_target, args=(item, indexer)))
+
+        for thread in threads:
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+        results = []
+
+        # Retrieve results from the queue and append them to the results list
+        while not results_queue.empty():
+            results.extend(results_queue.get())
+
+        # logger.info(results)
+
+        torrents = set()
+        correct_title = item.get_top_title()
+        if not correct_title:
+            logger.debug(f"Correct title not found for {item.log_string}")
+            return {}, 0
+
+        for item in results:
+            if item[1] is None or self.hash_cache.is_blacklisted(item[1]):
+                continue
+
+            try:
+                torrent: Torrent = self.rtn.rank(
+                    raw_title=item[0], infohash=item[1], correct_title=correct_title, remove_trash=True
+                )
+            except GarbageTorrent:
+                continue
             
-            scraped_torrents = sort_torrents(torrents)
-            return scraped_torrents, len(streams)
+            if torrent and torrent.fetch:
+                torrents.add(torrent)
+            
+        scraped_torrents = sort_torrents(torrents)
+        return scraped_torrents, len(scraped_torrents)
 
+    def _search_movie_indexer(self, item: MediaItem, indexer):
+        has_imdb_search_capability = (indexer.movie_search_capatabilities is not None and 'imdbid' in indexer.movie_search_capatabilities)
+
+        results = []
+
+        params = {
+            'apikey': self.api_key,
+            't': 'movie',
+            'cat': '2000',
+            'q': item.title,
+            'year': item.aired_at.year if hasattr(item.aired_at, "year") and item.aired_at.year else None
+        }
+
+        if has_imdb_search_capability:
+            params['imdbid'] = item.imdb_id
+
+        url = f"{self.settings.url}/api/v2.0/indexers/{indexer.id}/results/torznab/api"
+
+        try:
+            response = requests.get(url=url, params=params)
+            response.raise_for_status()
+            results = self._get_torrents_from_xml(response.text)
+        except Exception as e:
+            logger.error(
+                f"An exception occured while searching for a movie on Jackett with indexer {indexer.title}.")
+            logger.error(e)
+
+        return results
+
+    def _search_series_indexer(self, item: MediaItem, indexer):
+    
+        if item.type == "season":
+            q = item.parent.title
+            season = item.number
+            ep = None
+        elif item.type == "episode":
+            q = item.parent.parent.title
+            season = item.parent.number
+            ep = item.number
+
+
+        has_imdb_search_capability = (indexer.tv_search_capatabilities is not None
+                                      and 'imdbid' in indexer.tv_search_capatabilities)
+        
+
+        params = {
+            'apikey': self.__api_key,
+            't': 'tvsearch',
+            'cat': '5000',
+            'q': q,
+            'season': season,
+            'ep': ep
+        }
+
+        if has_imdb_search_capability:
+            params['imdbid'] = item.imdb_id
+
+        url = f"{self.settings.url}/api/v2.0/indexers/{indexer.id}/results/torznab/api"
+
+        try:
+            response = requests.get(url=url, params=params)
+            response.raise_for_status()
+
+            data = self._get_torrents_from_xml(response.text)
+            return data
+        except Exception:
+            logger.info(
+                f"An exception occured while searching for a series on Jackett with indexer {indexer.title}.")
+
+        return []
+
+
+    def _get_indexers(self):
+        url = f"{self.settings.url}/api/v2.0/indexers/all/results/torznab/api?apikey={self.api_key}&t=indexers&configured=true"
+
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            return self._get_indexer_from_xml(response.text)
+        except Exception as e:
+            logger.error("An exception occured while getting indexers from Jackett.")
+            logger.error(e)
+            return []
+
+    def _get_indexer_from_xml(self, xml_content):
+        xml_root = ET.fromstring(xml_content)
+
+        indexer_list = []
+        for item in xml_root.findall('.//indexer'):
+            indexer = JackettIndexer()
+
+            indexer.title = item.find('title').text
+            indexer.id = item.attrib['id']
+            indexer.link = item.find('link').text
+            indexer.type = item.find('type').text
+            indexer.language = item.find('language').text.split('-')[0]
+
+            logger.debug(f"Indexer: {indexer.title} - {indexer.link} - {indexer.type}")
+
+            movie_search = item.find('.//searching/movie-search[@available="yes"]')
+            tv_search = item.find('.//searching/tv-search[@available="yes"]')
+
+            if movie_search is not None:
+                indexer.movie_search_capatabilities = movie_search.attrib['supportedParams'].split(',')
+            else:
+                logger.debug(f"Movie search not available for {indexer.title}")
+
+            if tv_search is not None:
+                indexer.tv_search_capatabilities = tv_search.attrib['supportedParams'].split(',')
+            else:
+                logger.debug(f"TV search not available for {indexer.title}")
+
+            indexer_list.append(indexer)
+
+        return indexer_list
+
+    def _get_torrents_from_xml(self, xml_content):        
+        xml_root = ET.fromstring(xml_content)
+
+        result_list = []
+        for item in xml_root.findall('.//item'):
+            infoHash = item.find('.//torznab:attr[@name="infohash"]',
+                                 namespaces={'torznab': 'http://torznab.com/schemas/2015/feed'})
+
+            if infoHash is None:
+                continue
+
+            result_list.append((item.find('.//title').text, infoHash.attrib['value']))
+
+        return result_list
