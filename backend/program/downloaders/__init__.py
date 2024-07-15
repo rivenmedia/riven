fa0dc9532c17f0f094d28f7023c6ebc58354b277
Dreu LaVelle <dreu.lavelle@gmail.com>
2024-07-14 21:02:30 -0500

    fix: tidy initialize on fresh start

--- backend/program/downloaders/__init__.py
+++ backend/program/downloaders/__init__.py
@@ -8,16 +8,15 @@ class Downloader:
     def __init__(self, hash_cache):
         self.key = "downloader"
         self.initialized = False
-        services = [
+        self.services = [
             RealDebridDownloader(hash_cache),
             TorBoxDownloader(hash_cache),
             AllDebridDownloader(hash_cache),
         ]
-        self.service = next(service for service in services if service.initialized)
         self.initialized = self.validate()
 
     def validate(self):
-        return self.service is not None
+        return any(service.initialized for service in self.services)
 
     def run(self, item: MediaItem):
         yield next(self.service.run(item))
