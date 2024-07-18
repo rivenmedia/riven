57c569a8af6e054b7e6d8da512a4087cdb6dc348
Dreu LaVelle <dreu.lavelle@gmail.com>
2024-07-18 04:18:51 -0500

    chore!: removed frontend

--- frontend/src/routes/+error.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { page } from '$app/stores';
-</script>
-
-<div class="flex h-full w-full flex-col items-center justify-center font-primary font-medium">
-	<h1 class="text-center text-3xl font-bold">Something went wrong</h1>
-	<p class="text-base text-muted-foreground">Error code: {$page.status}</p>
-	<p class="text-sm text-muted-foreground">Error message: {$page.error?.message}</p>
-</div>
