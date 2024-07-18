c7c07a50c142273bc38b771ad2ba653bce7a633d
Dreu LaVelle <dreu.lavelle@gmail.com>
2024-07-18 05:44:50 -0500

    chore!: renamed backend dir to src. updated Dockerfile

---
+++ src/controllers/default.py
@@ -0,0 +1,164 @@
+import time
+
+import requests
+from fastapi import APIRouter, HTTPException, Request
+from program.media.item import MediaItem
+from program.content.trakt import TraktContent
+from program.media.state import States
+from program.scrapers import Scraping
+from program.settings.manager import settings_manager
+from program.media.item import Episode, MediaItem, Movie, Season, Show
+from program.db.db import db
+from sqlalchemy import select, func
+import program.db.db_functions as DB
+
+router = APIRouter(
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.get("/")
+async def root():
+    return {
+        "success": True,
+        "message": "Riven is running!",
+        "version": settings_manager.settings.version,
+    }
+
+
+@router.get("/health")
+async def health(request: Request):
+    return {
+        "success": True,
+        "message": request.app.program.initialized,
+    }
+
+
+@router.get("/rd")
+async def get_rd_user():
+    api_key = settings_manager.settings.downloaders.real_debrid.api_key
+    headers = {"Authorization": f"Bearer {api_key}"}
+
+    proxy = settings_manager.settings.downloaders.real_debrid.proxy_url if settings_manager.settings.downloaders.real_debrid.proxy_enabled else None
+
+    response = requests.get(
+        "https://api.real-debrid.com/rest/1.0/user",
+        headers=headers,
+        proxies=proxy if proxy else None,
+        timeout=10
+    )
+
+    if response.status_code != 200:
+        return {"success": False, "message": response.json()}
+
+    return {
+        "success": True,
+        "data": response.json(),
+    }
+
+
+@router.get("/torbox")
+async def get_torbox_user():
+    api_key = settings_manager.settings.downloaders.torbox.api_key
+    headers = {"Authorization": f"Bearer {api_key}"}
+    response = requests.get(
+        "https://api.torbox.app/v1/api/user/me", headers=headers, timeout=10
+    )
+    return response.json()
+
+
+@router.get("/services")
+async def get_services(request: Request):
+    data = {}
+    if hasattr(request.app.program, "services"):
+        for service in request.app.program.services.values():
+            data[service.key] = service.initialized
+            if not hasattr(service, "services"):
+                continue
+            for sub_service in service.services.values():
+                data[sub_service.key] = sub_service.initialized
+    return {"success": True, "data": data}
+
+
+@router.get("/trakt/oauth/initiate")
+async def initiate_trakt_oauth(request: Request):
+    trakt = request.app.program.services.get(TraktContent)
+    if trakt is None:
+        raise HTTPException(status_code=404, detail="Trakt service not found")
+    auth_url = trakt.perform_oauth_flow()
+    return {"auth_url": auth_url}
+
+
+@router.get("/trakt/oauth/callback")
+async def trakt_oauth_callback(code: str, request: Request):
+    trakt = request.app.program.services.get(TraktContent)
+    if trakt is None:
+        raise HTTPException(status_code=404, detail="Trakt service not found")
+    success = trakt.handle_oauth_callback(code)
+    if success:
+        return {"success": True, "message": "OAuth token obtained successfully"}
+    else:
+        raise HTTPException(status_code=400, detail="Failed to obtain OAuth token")
+
+
+@router.get("/stats")
+async def get_stats(request: Request):
+    payload = {}
+    with db.Session() as session:
+
+        movies_symlinks = session.execute(select(func.count(Movie._id)).where(Movie.symlinked == True)).scalar_one()
+        episodes_symlinks = session.execute(select(func.count(Episode._id)).where(Episode.symlinked == True)).scalar_one()
+        total_symlinks = movies_symlinks + episodes_symlinks
+
+        total_movies = session.execute(select(func.count(Movie._id))).scalar_one()
+        total_shows = session.execute(select(func.count(Show._id))).scalar_one()
+        total_seasons = session.execute(select(func.count(Season._id))).scalar_one()
+        total_episodes = session.execute(select(func.count(Episode._id))).scalar_one()
+        total_items = session.execute(select(func.count(MediaItem._id))).scalar_one()
+        _incomplete_items = session.execute(select(MediaItem).where(MediaItem.last_state != "Completed")).unique().scalars().all()
+
+        incomplete_retries = {}
+        for item in _incomplete_items:
+            incomplete_retries[item.log_string] = item.scraped_times
+
+        states = {}
+        for state in States:
+            states[state] = session.execute(select(func.count(MediaItem._id)).where(MediaItem.last_state == state.value)).scalar_one()
+
+        payload["total_items"] = total_items
+        payload["total_movies"] = total_movies
+        payload["total_shows"] = total_shows
+        payload["total_seasons"] = total_seasons
+        payload["total_episodes"] = total_episodes
+        payload["total_symlinks"] = total_symlinks
+        payload["incomplete_items"] = len(_incomplete_items)
+        payload["incomplete_retries"] = incomplete_retries
+        payload["states"] = states
+
+        return {"success": True, "data": payload}
+
+@router.get("/scrape/{item_id:path}")
+async def scrape_item(item_id: str, request: Request):
+    with db.Session() as session:
+        item = DB._get_item_from_db(session, MediaItem({"imdb_id":str(item_id)}))
+        if item is None:
+            raise HTTPException(status_code=404, detail="Item not found")
+        
+        scraper = request.app.program.services.get(Scraping)
+        if scraper is None:
+            raise HTTPException(status_code=404, detail="Scraping service not found")
+
+        time_now = time.time()
+        scraped_results = scraper.scrape(item, log=False)
+        time_end = time.time()
+        duration = time_end - time_now
+
+        results = {}
+        for hash, torrent in scraped_results.items():
+            results[hash] = {
+                "title": torrent.data.parsed_title,
+                "raw_title": torrent.raw_title,
+                "rank": torrent.rank,
+            }
+
+        return {"success": True, "total": len(results), "duration": round(duration, 3), "results": results}
---
+++ src/program/scrapers/knightcrawler.py
@@ -0,0 +1,118 @@
+""" Knightcrawler scraper module """
+from typing import Dict
+
+from program.media.item import Episode, MediaItem
+from program.scrapers.shared import _get_stremio_identifier
+from program.settings.manager import settings_manager
+from requests import ConnectTimeout, ReadTimeout
+from requests.exceptions import RequestException
+from utils.logger import logger
+from utils.request import get, ping
+from utils.ratelimiter import RateLimiter, RateLimitExceeded
+
+
+class Knightcrawler:
+    """Scraper for `Knightcrawler`"""
+
+    def __init__(self):
+        self.key = "knightcrawler"
+        self.settings = settings_manager.settings.scraping.knightcrawler
+        self.timeout = self.settings.timeout
+        self.initialized = self.validate()
+        if not self.initialized:
+            return
+        self.second_limiter = RateLimiter(max_calls=1, period=5) if self.settings.ratelimit else None
+        logger.success("Knightcrawler initialized!")
+
+    def validate(self) -> bool:
+        """Validate the Knightcrawler settings."""
+        if not self.settings.enabled:
+            logger.warning("Knightcrawler is set to disabled.")
+            return False
+        if not self.settings.url:
+            logger.error("Knightcrawler URL is not configured and will not be used.")
+            return False
+        if not isinstance(self.timeout, int) or self.timeout <= 0:
+            logger.error("Knightcrawler timeout is not set or invalid.")
+            return False
+        if not isinstance(self.settings.ratelimit, bool):
+            logger.error("Knightcrawler ratelimit must be a valid boolean.")
+            return False
+        try:
+            url = f"{self.settings.url}/{self.settings.filter}/manifest.json"
+            response = ping(url=url, timeout=self.timeout)
+            if response.is_ok:
+                return True
+        except Exception as e:
+            logger.error(f"Knightcrawler failed to initialize: {e}", )
+            return False
+        return True
+
+    def run(self, item: MediaItem) -> Dict[str, str]:
+        """Scrape the knightcrawler site for the given media items
+        and update the object with scraped streams"""
+        if not item:
+            return {}
+
+        try:
+            return self.scrape(item)
+        except RateLimitExceeded:
+            if self.second_limiter:
+                self.second_limiter.limit_hit()
+        except ConnectTimeout:
+            logger.warning(f"Knightcrawler connection timeout for item: {item.log_string}")
+        except ReadTimeout:
+            logger.warning(f"Knightcrawler read timeout for item: {item.log_string}")
+        except RequestException as e:
+            if e.response.status_code == 429:
+                if self.second_limiter:
+                    self.second_limiter.limit_hit()
+                else:
+                    logger.warning(f"Knightcrawler ratelimit exceeded for item: {item.log_string}")
+            else:
+                logger.error(f"Knightcrawler request exception: {e}")
+        except Exception as e:
+            logger.error(f"Knightcrawler exception thrown: {e}")
+        return {}
+
+    def scrape(self, item: MediaItem) -> Dict[str, str]:
+        """Scrape the given media item"""
+        data, stream_count = self.api_scrape(item)
+        if data:
+            logger.log("SCRAPER", f"Found {len(data)} streams out of {stream_count} for {item.log_string}")
+        else:
+            logger.log("NOT_FOUND", f"No streams found for {item.log_string}")
+        return data
+
+    def api_scrape(self, item: MediaItem) -> tuple[Dict[str, str], int]:
+        """Wrapper for `Knightcrawler` scrape method"""
+        identifier, scrape_type, imdb_id = _get_stremio_identifier(item)
+
+        url = f"{self.settings.url}/{self.settings.filter}/stream/{scrape_type}/{imdb_id}"
+        if identifier:
+            url += identifier
+
+        if self.second_limiter:
+            with self.second_limiter:
+                response = get(f"{url}.json", timeout=self.timeout)
+        else:
+            response = get(f"{url}.json", timeout=self.timeout)
+
+        if not response.is_ok or len(response.data.streams) <= 0:
+            return {}, 0
+
+        torrents: Dict[str, str] = {}
+
+        for stream in response.data.streams:
+            if not stream.infoHash:
+                continue
+
+            # For Movies and Episodes, we want the file name instead of the torrent title
+            # This should help with Special episodes and other misc. names
+            stream_title = stream.title.split("\n")[:-1]
+            joined_title = "\n".join(stream_title)
+            raw_title = joined_title.split("/")[-1] if isinstance(item, Episode) else joined_title.split("\n")[0]
+
+            torrents[stream.infoHash] = raw_title
+
+        return torrents, len(response.data.streams)
