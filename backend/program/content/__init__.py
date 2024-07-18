a167c412c683ab39b7b4007fe4c2e5b27bcbf1c2
Dreu LaVelle <dreu.lavelle@gmail.com>
2024-07-18 05:46:28 -0500

    fix: out with old :)

--- backend/program/content/__init__.py
+++
@@ -1,5 +0,0 @@
-from .listrr import Listrr
-from .mdblist import Mdblist
-from .overseerr import Overseerr
-from .plex_watchlist import PlexWatchlist
-from .trakt import TraktContent
--- backend/program/media/__init__.py
+++
@@ -1,2 +0,0 @@
-from .item import Episode, MediaItem, Movie, Season, Show  # noqa
-from .state import States  # noqa
