e1001da4c5fff7846441d451947be8c9890eda69
Robert Broketa <robert@broketa.com>
2024-07-19 01:51:51 -0300

    update type hints

--- src/program/types.py
+++ src/program/types.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Generator, Union
+from typing import Generator, Tuple, Union
 
 from program.content import Listrr, Mdblist, Overseerr, PlexWatchlist, TraktContent
 from program.downloaders import RealDebridDownloader, TorBoxDownloader, AllDebridDownloader
@@ -17,15 +17,15 @@ from program.scrapers import (
 )
 from program.scrapers.torbox import TorBoxScraper
 from program.symlink import Symlinker
-from program.updaters import Updater
+from program.updaters import Updater, OverseerrUpdater
 
 # Typehint classes
 Scraper = Union[Scraping, Torrentio, Knightcrawler, Mediafusion, Orionoid, Jackett, Annatar, TorBoxScraper, Zilean]
 Content = Union[Overseerr, PlexWatchlist, Listrr, Mdblist, TraktContent]
 Downloader = Union[RealDebridDownloader, TorBoxDownloader, AllDebridDownloader]
-Service = Union[Content, SymlinkLibrary, Scraper, Downloader, Symlinker, Updater]
+Service = Union[Content, SymlinkLibrary, Scraper, Downloader, Symlinker, Updater, OverseerrUpdater]
 MediaItemGenerator = Generator[MediaItem, None, MediaItem | None]
-ProcessedEvent = (MediaItem, Service, list[MediaItem])
+ProcessedEvent = Tuple[MediaItem, Service, list[MediaItem]]
 
 
 @dataclass
