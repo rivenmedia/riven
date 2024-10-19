4164372365cea36e6cc997b85b3d6b268408a1e2
Ayush Sehrawat <69469790+AyushSehrawat@users.noreply.github.com>
2024-10-19 14:54:53 +0000

    chore: remove unused tmdb api router

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -7,7 +7,7 @@ from routers.secure.default import router as default_router
 from routers.secure.items import router as items_router
 from routers.secure.scrape import router as scrape_router
 from routers.secure.settings import router as settings_router
-from routers.secure.tmdb import router as tmdb_router
+# from routers.secure.tmdb import router as tmdb_router
 from routers.secure.webhooks import router as webooks_router
 from routers.secure.ws import router as ws_router
 
@@ -25,6 +25,6 @@ app_router.include_router(default_router, dependencies=[Depends(resolve_api_key)
 app_router.include_router(items_router, dependencies=[Depends(resolve_api_key)])
 app_router.include_router(scrape_router, dependencies=[Depends(resolve_api_key)])
 app_router.include_router(settings_router, dependencies=[Depends(resolve_api_key)])
-app_router.include_router(tmdb_router, dependencies=[Depends(resolve_api_key)])
+# app_router.include_router(tmdb_router, dependencies=[Depends(resolve_api_key)])
 app_router.include_router(webooks_router, dependencies=[Depends(resolve_api_key)])
 app_router.include_router(ws_router, dependencies=[Depends(resolve_api_key)])
