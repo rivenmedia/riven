441195914b8df4467afbe6a8727d4b5bba1ddac1
eversio <github@burninglava.com>
2024-11-29 20:58:14 +1100

    test: fix alldebrid downloader tests

--- src/tests/test_alldebrid_downloader.py
+++ src/tests/test_alldebrid_downloader.py
@@ -1,136 +1,102 @@
 import json
 
 import pytest
+from unittest.mock import patch
 
 from program.services.downloaders import alldebrid
 from program.services.downloaders.alldebrid import (
     AllDebridDownloader,
-    add_torrent,
-    get_instant_availability,
-    get_status,
-    get_torrents,
+)
+from program.services.downloaders.models import (
+    VIDEO_EXTENSIONS,
+    DebridFile,
+    TorrentContainer,
+    TorrentInfo,
+    DownloadStatus,
 )
 from program.settings.manager import settings_manager as settings
+from program.utils.request import HttpMethod
 
 
 @pytest.fixture
-def downloader(instant, upload, status, status_all, delete):
+def downloader(user, upload, status, status_all, files, delete):
     """Instance of AllDebridDownloader with API calls mocked"""
-    # mock API calls
-    _get = alldebrid.get
-    def get(url, **params):
-        match url:
+
+    def execute(self, method: HttpMethod, endpoint: str, **kwargs) -> dict:
+        params = kwargs.get("params", {})
+        match endpoint:
             case "user":
-                return {"data": { "user": { "isPremium": True, "premiumUntil": 1735514599, } } }
-            case "magnet/instant":
-                return instant(url, **params)
+                return user()["data"]
             case "magnet/upload":
-                return upload(url, **params)
+                return upload(endpoint, **params)["data"]
             case "magnet/delete":
-                return delete(url, **params)
+                return delete(endpoint, **params)["data"]
             case "magnet/status":
                 if params.get("id", False):
-                    return status(url, **params)
+                    return status(endpoint, **params)["data"]
                 else:
-                    return status_all(url, **params)
+                    return status_all(endpoint, **params)["data"]
+            case "magnet/files":
+                return files(endpoint, **params)["data"]
             case _:
-                raise Exception("unmatched api call")
-    alldebrid.get = get
+                raise Exception("unmatched api call %s" % endpoint)
 
-    alldebrid_settings = settings.settings.downloaders.all_debrid
-    alldebrid_settings.enabled = True
-    alldebrid_settings.api_key = "key"
+    with patch.object(alldebrid.AllDebridRequestHandler, 'execute', execute):
+        alldebrid_settings = settings.settings.downloaders.all_debrid
+        alldebrid_settings.enabled = True
+        alldebrid_settings.api_key = "key"
 
-    downloader = AllDebridDownloader()
-    assert downloader.initialized
-    yield downloader
-
-    # tear down mock
-    alldebrid.get = get
+        downloader = AllDebridDownloader()
+        assert downloader.initialized
+        yield downloader
 
 
-## Downloader tests
-def test_process_hashes(downloader):
-    hashes = downloader.process_hashes(["abc"], None, [False, True])
-    assert len(hashes) == 1
+## DownloaderBase tests
+def test_validate(downloader):
+    assert downloader.validate() == True
 
 
-def test_download_cached(downloader):
-    torrent_id = downloader.download_cached({"infohash": "abc"})
-    assert torrent_id == MAGNET_ID
+def test_get_instant_availability(downloader):
+    assert downloader.get_instant_availability(MAGNET, "movie") == TorrentContainer(
+        infohash=MAGNET,
+        files=[DebridFile.create(filename="", filesize_bytes=123, filetype="movie")],
+    )
 
 
-def test_get_torrent_names(downloader):
-    names = downloader.get_torrent_names(123)
-    assert names == ("Ubuntu 24.04", None)
+def test_add_torrent(downloader):
+    assert downloader.add_torrent(MAGNET) == ID
 
 
-## API parsing tests
-def test_get_instant_availability(instant):
-    alldebrid.get = instant
-    infohashes = [UBUNTU]
-    availability = get_instant_availability(infohashes)
-    assert len(availability[0].get("files", [])) == 2
+def test_select_files(downloader):
+    assert downloader.select_files(ID, [1, 2, 3]) is None
 
 
-def test_get_instant_availability_unavailable(instant_unavailable):
-    alldebrid.get = instant_unavailable
-    infohashes = [UBUNTU]
-    availability = get_instant_availability(infohashes)
-    assert availability[0]["hash"] == UBUNTU
+def test_get_torrent_info(downloader):
+    torrent_info = downloader.get_torrent_info(ID)
+    assert torrent_info.id == int(ID)
+    assert torrent_info.name == "Big Buck Bunny"
+    assert torrent_info.status == DownloadStatus.READY
+    assert torrent_info.bytes == 276445467
+    assert torrent_info.progress == 100
 
 
-def test_add_torrent(upload):
-    alldebrid.get = upload
-    torrent_id = add_torrent(UBUNTU)
-    assert torrent_id == 251993753
+def test_delete_torrent(downloader):
+    assert downloader.delete_torrent(ID) is None  # TODO: assert that delete was called
 
 
-def test_add_torrent_cached(upload_ready):
-    alldebrid.get = upload_ready
-    torrent_id = add_torrent(UBUNTU)
-    assert torrent_id == 251993753
+# MAGNET is the infohash of the torrent.
+MAGNET = "dd8255ecdc7ca55fb0bbf81323d87062db1f6d1c"
+# ID is AllDebrid's internal ID for this torrent.
+ID = "264640947"
 
-
-def test_get_status(status):
-    alldebrid.get = status
-    torrent_status = get_status(251993753)
-    assert torrent_status["filename"] == "Ubuntu 24.04"
-
-
-def test_get_status_unfinished(status_downloading):
-    alldebrid.get = status_downloading
-    torrent_status = get_status(251993753)
-    assert torrent_status["status"] == "Downloading"
-
-
-def test_get_torrents(status_all):
-    alldebrid.get = status_all
-    torrents = get_torrents()
-    assert torrents[0]["status"] == "Ready"
-
-
-def test_delete(delete):
-    alldebrid.get = delete
-    delete(123)
-
-
-# Example requests - taken from real API calls
-UBUNTU = "3648baf850d5930510c1f172b534200ebb5496e6"
-MAGNET_ID = "251993753"
-@pytest.fixture
-def instant():
-    """GET /magnet/instant?magnets[0]=infohash (torrent available)"""
-    with open("src/tests/test_data/alldebrid_magnet_instant.json") as f:
-        body = json.load(f)
-    return lambda url, **params: body
 
 @pytest.fixture
-def instant_unavailable():
-    """GET /magnet/instant?magnets[0]=infohash (torrent unavailable)"""
-    with open("src/tests/test_data/alldebrid_magnet_instant_unavailable.json") as f:
+def user():
+    """GET /user"""
+    with open("src/tests/test_data/alldebrid_user.json") as f:
         body = json.load(f)
-    return lambda url, **params: body
+    return lambda: body
+
 
 @pytest.fixture
 def upload():
@@ -139,12 +105,14 @@ def upload():
         body = json.load(f)
     return lambda url, **params: body
 
+
 @pytest.fixture
 def upload_ready():
     """GET /magnet/upload?magnets[]=infohash (torrent ready)"""
     with open("src/tests/test_data/alldebrid_magnet_upload_ready.json") as f:
         body = json.load(f)
     return lambda url, **params: body
+
 
 @pytest.fixture
 def status():
@@ -153,6 +121,7 @@ def status():
         body = json.load(f)
     return lambda url, **params: body
 
+
 @pytest.fixture
 def status_downloading():
     """GET /magnet/status?id=123 (debrid links not ready yet)"""
@@ -160,18 +129,30 @@ def status_downloading():
         body = json.load(f)
     return lambda url, **params: body
 
+
 @pytest.fixture
 def status_all():
     """GET /magnet/status (gets a list of all links instead of a single object)"""
     # The body is the same as a single item, but with all your magnets in a list.
     with open("src/tests/test_data/alldebrid_magnet_status_one_ready.json") as f:
         body = json.load(f)
-    return lambda url, **params: {"status": "success", "data": {"magnets": [body["data"]["magnets"]]}}
+    return lambda url, **params: {
+        "status": "success",
+        "data": {"magnets": [body["data"]["magnets"]]},
+    }
+
 
 @pytest.fixture
-def delete():
-    """GET /delete"""
-    with open("src/tests/test_data/alldebrid_magnet_delete.json") as f:
+def files():
+    """GET /magnet/files?id[]=123 (gets files and links for a magnet)"""
+    with open("src/tests/test_data/alldebrid_magnet_files.json") as f:
         body = json.load(f)
     return lambda url, **params: body
 
+
+@pytest.fixture
+def delete():
+    """GET /delete?id=123"""
+    with open("src/tests/test_data/alldebrid_magnet_delete.json") as f:
+        body = json.load(f)
+    return lambda url, **params: body
