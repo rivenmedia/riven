3504754f40b9dbeb923bd160ff1148707846ebd9
Ayush Sehrawat <69469790+AyushSehrawat@users.noreply.github.com>
2025-02-12 00:14:08 +0530

    fix: update .env names and fix SKIP_TRAKT_CACHE (#1001)
    * fix: update .env names and fix SKIP_TRAKT_CACHE
    
    * fix: remove bool()
    
    * chore: revert FORCE_ENV to RIVEN_FORCE_ENV

--- src/program/apis/trakt_api.py
+++ src/program/apis/trakt_api.py
@@ -51,7 +51,7 @@ class TraktAPI:
         self.oauth_redirect_uri = self.settings.oauth.oauth_redirect_uri
         rate_limit_params = get_rate_limit_params(max_calls=1000, period=300)
         trakt_cache = get_cache_params("trakt", 86400)
-        use_cache = not os.environ.get("SKIP_TRAKT_CACHE", False)
+        use_cache = os.environ.get("SKIP_TRAKT_CACHE", "false").lower() == "true"
         session = create_service_session(rate_limit_params=rate_limit_params, use_cache=use_cache, cache_params=trakt_cache)
         self.headers = {
             "Content-type": "application/json",
