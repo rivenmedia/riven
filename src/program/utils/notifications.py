5408d55a8c152e7ff0d61a00866b186059ab1eb4
Davide Marcoli <69892203+davidemarcoli@users.noreply.github.com>
2025-04-10 17:39:52 +0200

    fix: duplicate notifications being sent when using multiple service urls (#1059)

--- src/program/utils/notifications.py
+++ src/program/utils/notifications.py
@@ -17,28 +17,26 @@ riven_logo: Path = root_dir / "assets" / "riven-light.png"
 
 for service_url in settings.service_urls:
     try:
+        if "discord" in service_url:
+            service_url = f"{service_url}?format=markdown"
         ntfy.add(service_url)
     except Exception as e:
         logger.debug(f"Failed to add service URL {service_url}: {e}")
         continue
 
 
-def notification(title: str, body: str) -> None:
+def notify(title: str, body: str) -> None:
     """Send notifications to all services in settings."""
-    for url in settings.service_urls:
-        if "discord" in url:
-            url = f"{url}?format=markdown"
-        try:
-            ntfy.notify(
-                title=title,
-                body=body,
-            )
-        except Exception as e:
-            logger.debug(f"Failed to send notification to {url}: {e}")
-            continue
+    try:
+        ntfy.notify(
+            title=title,
+            body=body,
+        )
+    except Exception as e:
+        logger.debug(f"Failed to send notification: {e}")
 
-def _build_discord_notification(item: MediaItem) -> str:
-    """Build a discord notification for the given item using markdown that lists the files completed."""
+def _build_notification_body(item: MediaItem) -> str:
+    """Build a notification for the given item using markdown that lists the files completed."""
     notification_message = f"### [{item.type.title()}] **{item.log_string}** ({item.aired_at.year})\n"
     stream: Stream = next((stream for stream in item.streams if stream.infohash == item.active_stream.get("infohash")), None)
     if stream:
@@ -51,5 +49,5 @@ def notify_on_complete(item: MediaItem) -> None:
         return
 
     title = "Riven completed something!" if not settings.title else settings.title
-    body = _build_discord_notification(item)
-    notification(title, body)
+    body = _build_notification_body(item)
+    notify(title, body)
