7809bd0c63680af2d011c8366407ba93714398e6
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-12-09 12:43:35 +0000

    chore: fix analysis errors

--- src/program/services/scrapers/orionoid.py
+++ src/program/services/scrapers/orionoid.py
@@ -16,6 +16,7 @@ class OrionoidErrorResponse(BaseModel):
     class Result(BaseModel):
         status: str
         message: str
+        type: str
 
     result: Result
 
@@ -285,15 +286,15 @@ class Orionoid(ScraperService[OrionoidConfig]):
             timeout=self.timeout,
         )
 
-        if not response.ok:
-            logger.error(
-                f"Orionoid scrape failed for {item.log_string}: {response.text}"
-            )
-
-            return {}
-
         try:
-            OrionoidErrorResponse.model_validate(response.json())
+            error = OrionoidErrorResponse.model_validate(response.json())
+
+            if error.result.type == "streammissing":
+                logger.log("NOT_FOUND", f"No streams found for {item.log_string}")
+            else:
+                logger.error(
+                    f"Orionoid scrape failed for {item.log_string}: {response.text}"
+                )
 
             logger.error(
                 f"Orionoid scrape failed for {item.log_string}: {response.text}"
